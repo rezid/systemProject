9c38f817f231e9a62d429a4bf7c3edecf6ff987a
Rezzak Zidane <zidane.rezzak@gmail.com>
2017-06-07 07:34:20 -0500

    clean up mutex

--- src/conduct.c
+++ src/conduct.c
@@ -28,27 +28,15 @@ struct conduct
     char *data_map_ptr;    
 };
 
-struct element_t {
-    char element;
+struct sync_t {
     pthread_mutex_t mutex;
     pthread_cond_t cond_empty;
     pthread_cond_t cond_full;
 };
 
-struct conduct_size_t {
-    size_t size;
-    pthread_mutex_t mutex;
-};
-
-struct conduct_bool_t {
-    bool value;
-    pthread_mutex_t mutex;
-};
-
 int create_file_name (const char *name, char * out_conduct_file_name, char * out_data_file_name);
 size_t get_conduct_space(struct conduct *c);
 ssize_t initialize_header_information(char* conduct_map_ptr, size_t atomic_length, size_t real_global_length);
-ssize_t initialize_data_file(char* data_map_ptr, size_t real_global_length);
 size_t get_read_offset(char *conduct_map_ptr);
 size_t get_write_offset(char *conduct_map_ptr);
 size_t get_atomic_length(char *conduct_map_ptr);
@@ -63,11 +51,10 @@ const size_t buffer_size = 1024;
 struct conduct *conduct_create(const char *name, size_t atomic_length, size_t global_length)
 {
     char *conduct_map_ptr, *data_map_ptr;
-    size_t conduct_file_size = sizeof(struct conduct_size_t) * 4 + 2 * sizeof(struct conduct_bool_t);
-    size_t data_file_size = global_length * sizeof(struct element_t);
+    size_t conduct_file_size = sizeof(size_t) * 4 + 2 * sizeof(bool) + sizeof(struct sync_t);
+    size_t data_file_size = global_length * sizeof(char);
     
     struct conduct * out = malloc(sizeof(struct conduct));
-    
     
     // Error
     if (global_length < atomic_length)
@@ -175,17 +162,10 @@ struct conduct *conduct_create(const char *name, size_t atomic_length, size_t gl
     }
 
     // write header information to the mapped conduct file
-    if (initialize_header_information(conduct_map_ptr, atomic_length, data_file_size / sizeof(struct element_t)) == -1){
+    if (initialize_header_information(conduct_map_ptr, atomic_length, data_file_size / sizeof(char)) == -1){
         perror("initialize_header_information");
         return NULL;
     }
-
-    // initialize mutex and variable condition in mapped data file
-    if (initialize_data_file(data_map_ptr, data_file_size / sizeof(struct element_t)) == -1){
-        perror("initialize_header_information");
-        return NULL;
-    }
-
 
     // return the conduct structure
     out->conduct_map_ptr = conduct_map_ptr;
@@ -279,74 +259,73 @@ ssize_t conduct_write(struct conduct *c, const void *buf, size_t count)
     if (count == 0)
         return -1;
 
-    struct conduct_size_t * conduct_size = (struct conduct_size_t *) (c->conduct_map_ptr);
-    struct conduct_bool_t * conduct_bool = (struct conduct_bool_t *) (&conduct_size[4]);
-    struct element_t * element = (struct element_t *) (c->data_map_ptr);
+    size_t * conduct_size = (size_t *) (c->conduct_map_ptr);
+    bool * conduct_bool = (bool *) (&conduct_size[4]);
+    struct sync_t * conduct_sync = (struct sync_t *) (&conduct_bool[2]);
+    char * element = (char *) (c->data_map_ptr);
 
     // cast for array subscripting
     const char * buff = (const char *) buf;
 
-    pthread_mutex_lock(&(element[0].mutex));
+    pthread_mutex_lock(&(conduct_sync->mutex));
     
     DEBUG_PRINT("---------------------(Write)Print mmap conduct information---------------------\n");
-    DEBUG_PRINT("(DEBUG)read offset is: %zu\n", conduct_size[0].size);
-    DEBUG_PRINT("(DEBUG)write offset is: %zu\n", conduct_size[1].size);
-    DEBUG_PRINT("(DEBUG)atomic length is: %zu\n", conduct_size[2].size);
-    DEBUG_PRINT("(DEBUG)global length is: %zu\n", conduct_size[3].size);
-    DEBUG_PRINT("(DEBUG)eof is: %d\n", conduct_bool[0].value);
-    DEBUG_PRINT("(DEBUG)isEmpty is: %d\n", conduct_bool[1].value);
+    DEBUG_PRINT("(DEBUG)read offset is: %zu\n", conduct_size[0]);
+    DEBUG_PRINT("(DEBUG)write offset is: %zu\n", conduct_size[1]);
+    DEBUG_PRINT("(DEBUG)atomic length is: %zu\n", conduct_size[2]);
+    DEBUG_PRINT("(DEBUG)global length is: %zu\n", conduct_size[3]);
+    DEBUG_PRINT("(DEBUG)eof is: %d\n", conduct_bool[0]);
+    DEBUG_PRINT("(DEBUG)isEmpty is: %d\n", conduct_bool[1]);
     DEBUG_PRINT("(DEBUG)conduct free space is: %zu\n", get_conduct_space(c));
     DEBUG_PRINT("(DEBUG)count: %zu\n", count);
     DEBUG_PRINT("(DEBUG)done\n");
-    
-    sleep(0.25);
 
-    size_t a = conduct_size[2].size;
+    size_t a = conduct_size[2];
     size_t return_value = 0;
 
 
     if (count <= a) {
         // partiel write: bloque if not count Bytes free
         while (get_conduct_space(c) <= count) {   
-            if(conduct_bool[0].value == true) {
-                pthread_mutex_unlock(&(element[0].mutex));
+            if(conduct_bool[0] == true) {
+                pthread_mutex_unlock(&(conduct_sync->mutex));
                 errno = EPIPE;
                 return -1;
             }
-            pthread_cond_wait(&(element[0].cond_full), &(element[0].mutex));
+            pthread_cond_wait(&(conduct_sync->cond_full), &(conduct_sync->mutex));
         }
 
         // writing bytes
         for(unsigned int i = 0; i < count; ++i) {
-            element[conduct_size[1].size + i].element = buff[i];
+            element[conduct_size[1] + i] = buff[i];
         }
-        conduct_size[1].size = (conduct_size[1].size + count) % conduct_size[3].size;
+        conduct_size[1] = (conduct_size[1] + count) % conduct_size[3];
         return_value = count;
         // Wakeup all
-        pthread_cond_broadcast(&(element[0].cond_empty));    
+        pthread_cond_broadcast(&(conduct_sync->cond_empty));    
     }
     else if (count > a){
         // if not count Bytes free, then write anythink >= 1 Byte
         while (get_conduct_space(c) == 0) {
-            if(conduct_bool[0].value == true) {
-                pthread_mutex_unlock(&(element[0].mutex));
+            if(conduct_bool[0] == true) {
+                pthread_mutex_unlock(&(conduct_sync->mutex));
                 errno = EPIPE;
                 return -1;
             }  
-            pthread_cond_wait(&(element[0].cond_full), &(element[conduct_size[1].size].mutex));
+            pthread_cond_wait(&(conduct_sync->cond_full), &(conduct_sync->mutex));
         }
 
         // writing bytes
         for(unsigned int i = 0; i< get_conduct_space(c); ++i) {
-            element[conduct_size[1].size + i].element = buff[i];
+            element[conduct_size[1] + i] = buff[i];
         }
-        conduct_size[1].size = (conduct_size[1].size + get_conduct_space(c)) % conduct_size[3].size;
+        conduct_size[1] = (conduct_size[1] + get_conduct_space(c)) % conduct_size[3];
         return_value = get_conduct_space(c);
         // Wakeup all
-        pthread_cond_broadcast(&(element[0].cond_empty)); 
+        pthread_cond_broadcast(&(conduct_sync->cond_empty)); 
     }
 
-    pthread_mutex_unlock(&(element[0].mutex));
+    pthread_mutex_unlock(&(conduct_sync->mutex));
     return return_value;
 }
 
@@ -358,62 +337,61 @@ ssize_t conduct_read(struct conduct *c, void *buf, size_t count)
         return -1;
     if (count == 0)
         return -1;
-
-    struct conduct_size_t * conduct_size = (struct conduct_size_t *) (c->conduct_map_ptr);
-    struct conduct_bool_t * conduct_bool = (struct conduct_bool_t *) (&conduct_size[4]);
-    struct element_t * element = (struct element_t *) (c->data_map_ptr);
+    
+    size_t * conduct_size = (size_t *) (c->conduct_map_ptr);
+    bool * conduct_bool = (bool *) (&conduct_size[4]);
+    struct sync_t * conduct_sync = (struct sync_t *) (&conduct_bool[2]);
+    char * element = (char *) (c->data_map_ptr);
 
     // cast for array subscripting
     char * buff = (char *) buf;
 
-    pthread_mutex_lock(&(element[0].mutex));
+    pthread_mutex_lock(&(conduct_sync->mutex));
     
     DEBUG_PRINT("---------------------(Read)Print mmap conduct information---------------------\n");
-    DEBUG_PRINT("(DEBUG)read offset is: %zu\n", conduct_size[0].size);
-    DEBUG_PRINT("(DEBUG)write offset is: %zu\n", conduct_size[1].size);
-    DEBUG_PRINT("(DEBUG)atomic length is: %zu\n", conduct_size[2].size);
-    DEBUG_PRINT("(DEBUG)global length is: %zu\n", conduct_size[3].size);
-    DEBUG_PRINT("(DEBUG)eof is: %d\n", conduct_bool[0].value);
-    DEBUG_PRINT("(DEBUG)isEmpty is: %d\n", conduct_bool[1].value);
+    DEBUG_PRINT("(DEBUG)read offset is: %zu\n", conduct_size[0]);
+    DEBUG_PRINT("(DEBUG)write offset is: %zu\n", conduct_size[1]);
+    DEBUG_PRINT("(DEBUG)atomic length is: %zu\n", conduct_size[2]);
+    DEBUG_PRINT("(DEBUG)global length is: %zu\n", conduct_size[3]);
+    DEBUG_PRINT("(DEBUG)eof is: %d\n", conduct_bool[0]);
+    DEBUG_PRINT("(DEBUG)isEmpty is: %d\n", conduct_bool[1]);
     DEBUG_PRINT("(DEBUG)conduct free space is: %zu\n", get_conduct_space(c));
     DEBUG_PRINT("(DEBUG)count: %zu\n", count);
     DEBUG_PRINT("(DEBUG)done\n");
-    
-    sleep(0.25);
 
     size_t return_value = 0;
 
-    while(get_conduct_space(c) == conduct_size[3].size) {
-        if (conduct_bool[0].value == true) {
-            pthread_mutex_unlock(&(element[0].mutex));
+    while(get_conduct_space(c) == conduct_size[3]) {
+        if (conduct_bool[0] == true) {
+            pthread_mutex_unlock(&(conduct_sync->mutex));
             return 0;
         }
-        pthread_cond_wait(&(element[0].cond_empty), &(element[0].mutex));
+        pthread_cond_wait(&(conduct_sync->cond_empty), &(conduct_sync->mutex));
     }
 
-    if (count <= conduct_size[3].size - get_conduct_space(c)) {
+    if (count <= conduct_size[3] - get_conduct_space(c)) {
         // reading bytes
         for(unsigned int i = 0; i< count; ++i) {
-            buff[i] = element[conduct_size[0].size + i].element;
+            buff[i] = element[conduct_size[0] + i];
         }
-        conduct_size[0].size = (conduct_size[0].size + count) % conduct_size[3].size;
+        conduct_size[0] = (conduct_size[0] + count) % conduct_size[3];
         return_value = count;
         // Wakeup all
-        pthread_cond_broadcast(&(element[0].cond_full));    
+        pthread_cond_broadcast(&(conduct_sync->cond_full));    
     }
     else {
-        int number = conduct_size[3].size - get_conduct_space(c);
+        int number = conduct_size[3] - get_conduct_space(c);
         // reading bytes
         for(int i = 0; i< number; ++i) {
-             buff[i] = element[conduct_size[0].size + i].element;
+             buff[i] = element[conduct_size[0] + i];
         }
-        conduct_size[0].size = (conduct_size[0].size + number) % conduct_size[3].size;
+        conduct_size[0] = (conduct_size[0] + number) % conduct_size[3];
         return_value = number;
         // Wakeup all
-        pthread_cond_broadcast(&(element[0].cond_full)); 
+        pthread_cond_broadcast(&(conduct_sync->cond_full)); 
     }
 
-    pthread_mutex_unlock(&(element[0].mutex));
+    pthread_mutex_unlock(&(conduct_sync->mutex));
     return return_value;
 }
 
@@ -423,16 +401,17 @@ int conduct_write_eof(struct conduct *c)
     if (c->conduct_map_ptr == NULL || c->data_map_ptr == NULL)
         return -1;
  
-    struct conduct_size_t * conduct_size = (struct conduct_size_t *) (c->conduct_map_ptr);
-    struct conduct_bool_t * conduct_bool = (struct conduct_bool_t *) (&conduct_size[4]);
-    struct element_t * element = (struct element_t *) (c->data_map_ptr);
+    size_t * conduct_size = (size_t *) (c->conduct_map_ptr);
+    bool * conduct_bool = (bool *) (&conduct_size[4]);
+    struct sync_t * conduct_sync = (struct sync_t *) (&conduct_bool[2]);
 
-    if (conduct_bool[1].value == true)
+
+    if (conduct_bool[1] == true)
         return 0;
 
-    pthread_mutex_lock(&(element[0].mutex));
-    conduct_bool[0].value = true;
-    pthread_mutex_unlock(&(element[0].mutex));
+    pthread_mutex_lock(&(conduct_sync->mutex));
+    conduct_bool[0] = true;
+    pthread_mutex_unlock(&(conduct_sync->mutex));
     return 0;
 }
 
@@ -457,10 +436,10 @@ void conduct_close(struct conduct *conduct)
     if (conduct->conduct_map_ptr == NULL || conduct->data_map_ptr == NULL)
         return;
    
-    struct conduct_size_t * conduct_size = (struct conduct_size_t *) (conduct->conduct_map_ptr);
+    size_t * conduct_size = (size_t *) (conduct->conduct_map_ptr);
 
-    munmap(conduct->conduct_map_ptr, 4 * sizeof(struct conduct_size_t) + 2 * sizeof(struct conduct_bool_t));
-    munmap(conduct->data_map_ptr, conduct_size[3].size * sizeof(struct element_t));
+    munmap(conduct->conduct_map_ptr, 4 * sizeof(size_t) + 2 * sizeof(bool) + sizeof(struct sync_t));
+    munmap(conduct->data_map_ptr, conduct_size[3] * sizeof(char));
 }
 
 void conduct_destroy(struct conduct *conduct)
@@ -487,13 +466,13 @@ void conduct_destroy(struct conduct *conduct)
 
 size_t get_conduct_space(struct conduct *c)
 {
-    struct conduct_size_t * conduct_size = (struct conduct_size_t *) (c->conduct_map_ptr);
-    struct conduct_bool_t * conduct_bool = (struct conduct_bool_t *) (&conduct_size[4]);
+    size_t * conduct_size = (size_t *) (c->conduct_map_ptr);
+    bool * conduct_bool = (bool *) (&conduct_size[4]);
 
-    bool data_isEmpty = conduct_bool[1].value;
-    size_t read_offset = conduct_size[0].size;
-    size_t write_offset = conduct_size[1].size;
-    size_t global_length = conduct_size[3].size;
+    bool data_isEmpty = conduct_bool[1];
+    size_t read_offset = conduct_size[0];
+    size_t write_offset = conduct_size[1];
+    size_t global_length = conduct_size[3];
 
     if (read_offset == write_offset) {
         if (data_isEmpty)
@@ -510,70 +489,52 @@ ssize_t initialize_header_information(char* conduct_map_ptr, size_t a, size_t re
     DEBUG_PRINT("------Create header information------\n");
     int err;
     pthread_mutexattr_t mutex_attr;
+    pthread_condattr_t cond_attr;
     err = pthread_mutexattr_init(&mutex_attr); if (err) return -1;
+    err = pthread_condattr_init(&cond_attr); if (err) return -1;
     err = pthread_mutexattr_setpshared(&mutex_attr, PTHREAD_PROCESS_SHARED); if (err) return -1;
-    struct conduct_size_t read_offset = {.size = 1};
-    struct conduct_size_t write_offset = {.size = 1};
-    struct conduct_size_t atomic_length = {.size = a};
-    struct conduct_size_t global_length = {.size = real_global_length};
-    struct conduct_bool_t eof_insered = {.value = false};
-    struct conduct_bool_t data_isEMpty = {.value = true};
-    err = pthread_mutex_init(&(read_offset.mutex), &mutex_attr); if (err) return -1;
-    err = pthread_mutex_init(&(write_offset.mutex), &mutex_attr); if (err) return -1;
-    err = pthread_mutex_init(&(atomic_length.mutex), &mutex_attr); if (err) return -1;
-    err = pthread_mutex_init(&(global_length.mutex), &mutex_attr); if (err) return -1;
-    err = pthread_mutex_init(&(eof_insered.mutex), &mutex_attr); if (err) return -1;
-    err = pthread_mutex_init(&(data_isEMpty.mutex), &mutex_attr); if (err) return -1;
+    err = pthread_condattr_setpshared(&cond_attr, PTHREAD_PROCESS_SHARED); if (err) return -1;
+
+    size_t read_offset = 1;
+    size_t write_offset = 1;
+    size_t atomic_length = a;
+    size_t global_length = real_global_length;
+    bool eof_insered = false;
+    bool data_isEMpty = true;
+    struct sync_t conduct_struct;
+
+    err = pthread_mutex_init(&(conduct_struct.mutex), &mutex_attr); if (err) return -1;
+    err = pthread_cond_init(&(conduct_struct.cond_empty), &cond_attr); if (err) return -1;
+    err = pthread_cond_init(&(conduct_struct.cond_full), &cond_attr); if (err) return -1;
+ 
     DEBUG_PRINT("(DEBUG)done\n");
 
     DEBUG_PRINT("------Writing header information to the mapped file------\n");
-    struct conduct_size_t * conduct_size = (struct conduct_size_t *) (conduct_map_ptr);
-    struct conduct_bool_t * conduct_bool = (struct conduct_bool_t *) (&conduct_size[4]);
+    size_t * conduct_size = (size_t *) (conduct_map_ptr);
+    bool * conduct_bool = (bool *) (&conduct_size[4]);
+    struct sync_t * conduct_sync = (struct sync_t *) (&conduct_bool[2]);
    
     conduct_size[0] = read_offset;
-    DEBUG_PRINT("(DEBUG)write 'read_offset': %zu\n", read_offset.size);
+    DEBUG_PRINT("(DEBUG)write 'read_offset': %zu\n", read_offset);
 
     conduct_size[1] = write_offset;
-    DEBUG_PRINT("(DEBUG)write 'write_offset': %zu\n", write_offset.size);
+    DEBUG_PRINT("(DEBUG)write 'write_offset': %zu\n", write_offset);
 
     conduct_size[2] = atomic_length;
-    DEBUG_PRINT("(DEBUG)write 'atomic_length': %zu\n", atomic_length.size);
+    DEBUG_PRINT("(DEBUG)write 'atomic_length': %zu\n", atomic_length);
 
     conduct_size[3] = global_length;
-    DEBUG_PRINT("(DEBUG)write 'global_length: %zu\n", global_length.size);
+    DEBUG_PRINT("(DEBUG)write 'global_length: %zu\n", global_length);
 
     conduct_bool[0] = eof_insered;
-    DEBUG_PRINT("(DEBUG)write 'eof_insered': %d\n", eof_insered.value);
+    DEBUG_PRINT("(DEBUG)write 'eof_insered': %d\n", eof_insered);
 
     conduct_bool[1] = data_isEMpty;
-    DEBUG_PRINT("(DEBUG)write 'data_isEMpty': %d\n", data_isEMpty.value);
+    DEBUG_PRINT("(DEBUG)write 'data_isEMpty': %d\n", data_isEMpty);
+
+    conduct_sync[0] = conduct_struct;
+    DEBUG_PRINT("(DEBUG)write mutex ans var cond information\n");
     DEBUG_PRINT("(DEBUG)done\n");
     return 0;
 }
-
-ssize_t initialize_data_file(char* data_map_ptr, size_t real_global_length)
-{
-    DEBUG_PRINT("------Create mutex and condition varriable------\n");
-    struct element_t * element_array = (struct element_t *) (data_map_ptr);
-    int err;
-    for(unsigned int i = 0; i < real_global_length; ++i) {
-        pthread_mutexattr_t mutex_attr;
-        pthread_condattr_t cond_attr;
-        err = pthread_mutexattr_init(&mutex_attr); if (err) return -1;
-        err = pthread_condattr_init(&cond_attr); if (err) return -1;
-        err = pthread_mutexattr_setpshared(&mutex_attr, PTHREAD_PROCESS_SHARED); if (err) return -1;
-        err = pthread_condattr_setpshared(&cond_attr, PTHREAD_PROCESS_SHARED); if (err) return -1;
-        struct element_t element = {.element = 0};
-        err = pthread_mutex_init(&(element.mutex), &mutex_attr); if (err) return -1;
-        err = pthread_cond_init(&(element.cond_empty), &cond_attr); if (err) return -1;
-        err = pthread_cond_init(&(element.cond_full), &cond_attr); if (err) return -1;
-
-        element_array[i] = element;
-    }
-    DEBUG_PRINT("(DEBUG)done\n");
-    return 0;
-}
-
-
-
 
